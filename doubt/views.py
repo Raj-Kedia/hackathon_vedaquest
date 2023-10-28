934c3d14cb0e09e9b429b815dbcee4498dd0917b
Raj Kedia <rajramkola786@gmail.com>
2023-10-28 16:28:55 +0530

    final commit for today

--- doubt/views.py
+++ doubt/views.py
@@ -8,27 +8,4 @@ from django.contrib.auth.models import User
 def doubthome(request):
     allPosts = doubt.objects.all()
     context = {'allPosts': allPosts}
-    return render(request, "blog/blogHome.html", context)
-
-
-def postComment(request):
-    if request.method == "POST":
-        comment = request.POST.get('comment')
-        user = request.user
-        postSno = request.POST.get('postSno')
-        post = doubt.objects.get(sno=postSno)
-        parentSno = request.POST.get('parentSno')
-        if parentSno == "":
-            comment = Answer(comment=comment, user=user, post=post)
-            comment.save()
-            messages.success(
-                request, "Your comment has been posted successfully")
-        else:
-            parent = Answer.objects.get(sno=parentSno)
-            comment = Answer(
-                comment=comment, user=user, post=post, parent=parent)
-            comment.save()
-            messages.success(
-                request, "Your reply has been posted successfully")
-
-    return redirect(f"/blog/{post.slug}")
+    return HttpResponse("Coming soon....")
